0e4497e02e87f5199a54e8fc74a8396abb881755
Fisch37 <seracin4@gmail.com>
2026-02-15 20:29:29 +0100

    Added a lot of documentation to groupie base module

--- src/groupie.rs
+++ src/groupie.rs
@@ -1,3 +1,10 @@
+//! Groupie is the central sensor managing API.
+//! This project collects all sensor data in a single [`SensorStorage`],
+//! where all enabled [`SensorPlugin`]s are also immediately initialised.
+//!
+//! This module also contains the datastructures necessary to work with sensor data,
+//! most notably the [`SensorKey`] trait and the [`OwnedKey`] struct.
+
 use std::{array, cmp::Ordering, error::Error, fmt::Display, hash::Hash, rc::Rc, time::Instant};
 
 use hashbrown::{Equivalent, HashMap};
@@ -14,6 +21,8 @@ mod lm_sensors;
 mod nvidia;
 mod nvml;
 
+/// A helper struct for displaying a [`SensorKey`].
+/// Used in the [`SensorKey::display`] method.
 pub struct KeyWriter<'a>(&'a str, &'a str);
 impl<'a> Display for KeyWriter<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -21,13 +30,28 @@ impl<'a> Display for KeyWriter<'a> {
     }
 }
 
+/// A sensor key is any datastructure that can be used to uniquely identify a [`Sensor`].
+///
+/// Two things uniquely identify a sensor: The key to its adapter and its name.
+/// As such, the core of this trait is the [`Self::get_sensor_key`] method,
+/// which retrieves this pair of information.
 pub trait SensorKey {
+    /// Get the uniquely identifying sensor key contained in this datastructure.
+    ///
+    /// The first value is the adapter key and the second the sensor name.
     fn get_sensor_key(&self) -> (&str, &str);
 
+    /// Like [`Self::get_sensor_key`], but returns an [`OwnedKey`] instead.
     fn get_owned_key(&self) -> OwnedKey {
         self.get_sensor_key().into()
     }
 
+    /// Comparator method providing a total ordering on two [`SensorKey`]
+    /// instances. The ordering is lexicographical with highest priority
+    /// on the adapter key, secondary comparison on the sensor name.
+    ///
+    /// This means that the following order would be correct:
+    /// (A, a), (A, b), (B, a), (B, a), (B, b)
     fn cmp_by_sensor_key<T: SensorKey>(&self, other: T) -> Ordering {
         let my_key = self.get_sensor_key();
         let other_key = other.get_sensor_key();
@@ -37,15 +61,22 @@ pub trait SensorKey {
         }
     }
 
+    /// Returns a helper for displaying a sensor key,
+    /// formatted as &lt;adapter key&gt;/&lt;sensor name&gt;.
+    ///
+    /// The intended use for this is to handle display properties
+    /// that cannot be implemented directly, due to ambiguities or Rust's orphaning rules.
     fn display(&self) -> KeyWriter<'_> {
         let (name, adapter) = self.get_sensor_key();
         KeyWriter(name, adapter)
     }
 
+    /// Returns only the second part of the sensor key.
     fn get_sensor_name(&self) -> &str {
         self.get_sensor_key().1
     }
 
+    /// Returns only the first part of the sensor key.
     fn get_adapter_key(&self) -> &str {
         self.get_sensor_key().0
     }
@@ -64,9 +95,15 @@ impl<T: SensorKey> SensorKey for &T {
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 // Hacky, probably causes an allocation on serialize; fixme?
 #[serde(from = "(String, String)", into = "(String, String)")]
+/// An owned implementor of [`SensorKey`].
 pub struct OwnedKey {
     pub adapter: String,
     pub name: String,
+}
+impl From<OwnedKey> for (String, String) {
+    fn from(value: OwnedKey) -> Self {
+        (value.adapter, value.name)
+    }
 }
 impl From<(String, String)> for OwnedKey {
     fn from(value: (String, String)) -> Self {
@@ -76,13 +113,12 @@ impl From<(String, String)> for OwnedKey {
         }
     }
 }
-impl From<OwnedKey> for (String, String) {
-    fn from(value: OwnedKey) -> Self {
-        (value.adapter, value.name)
-    }
-}
-impl<'a, 'b> From<(&'a str, &'b str)> for OwnedKey {
-    fn from(value: (&'a str, &'b str)) -> Self {
+impl<'a, 'b, A, B> From<(&'a A, &'b B)> for OwnedKey
+where 
+    A: AsRef<str> + ?Sized,
+    B: AsRef<str> + ?Sized
+{
+    fn from(value: (&'a A, &'b B)) -> Self {
         (value.0.to_owned(), value.1.to_owned()).into()
     }
 }
@@ -103,8 +139,17 @@ impl Equivalent<OwnedKey> for (&str, &str) {
 }
 
 #[derive(Debug, Eq)]
+/// An adapter (or chip, or bus, or device) is a logical object on which sensors can be found.
+/// The exact semantics of this are intentionally left up to each sensor plugin and Groupie itself
+/// makes no assumptions about what qualifies as an adapter.
 pub struct Adapter {
+    /// The key uniquely identifies this adapter.
+    /// Two different logical adapters should never be referred to by the same key
+    /// and doing so constitutes a logic error.
     pub key: String,
+    /// A human-readable name for this adapter.
+    /// This name need not be unique, though developers should strive to choose a readable name.
+    /// (e.g. NVML uses the name of the GPU as the adapter name)
     pub name: String,
 }
 impl PartialEq for Adapter {
@@ -129,6 +174,7 @@ impl Display for Adapter {
 }
 
 #[derive(Debug, Clone)]
+/// A merged form of [`Sensor`] and [`SensorState`].
 pub struct SensorData<'a> {
     pub name: &'a str,
     pub input: f64,
@@ -183,6 +229,15 @@ impl<'a> From<&'a (Sensor, SensorState)> for SensorData<'a> {
 #[strum(serialize_all = "kebab-case")]
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
+/// The kind of value a sensor is storing.
+/// This also corresponds to a particular unit.
+/// 
+/// Units are kept in their "everyday" format,
+/// but scaled down to their base unit where applicable.
+/// (e.g. [`Self::Fan`] uses RPM, but [`Self::Power`] uses watts, not milliwatts)
+/// 
+/// It is highly recommendable for plugin developers to look at the specified units
+/// and scale the values received by their datasource accordingly.
 pub enum SensorKind {
     Temperature,
     Fan,
@@ -191,6 +246,21 @@ pub enum SensorKind {
     Voltmeter,
     Current,
     Energy,
+}
+impl SensorKind {
+    /// Get the unit affix for this kind of sensor.
+    pub fn get_unit(self) -> &'static str {
+        match self {
+            Self::Temperature => "°C",
+            Self::Fan => "RPM",
+            Self::Beep => "", // 1 BEEEEEP, 2 BEEEEEP, 3 BEEEEEEP, ...
+            Self::Power => "W",
+            Self::Voltmeter => "V",
+            Self::Current => "A",
+            // Energy is open to discussion and may be better used given in "kWh"
+            Self::Energy => "J"
+        }
+    }
 }
 impl Display for SensorKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -212,8 +282,8 @@ pub struct SensorState {
     pub input: f64,
     pub min: f64,
     pub max: f64,
-    // Using a u32 here ensures our SensorState is 32 bytes long, instead of the 40 if we had used Instant
     /// A monotonic clock value, marking the time at which this state was constructed.
+    // Using a u32 here ensures our SensorState is 32 bytes long, instead of the 40 if we had used Instant
     pub polled_at: u32,
 }
 impl SensorState {
@@ -234,6 +304,9 @@ impl AsRef<SensorState> for SensorState {
 }
 
 #[derive(Debug, Clone)]
+/// The unchanging metadata about a sensor.
+/// Collected by [`SensorStorage`] during the first [`SensorStorage::update`] call
+/// and expected to be permanent for the entire runtime of the program.
 pub struct Sensor {
     pub name: String,
     pub adapter: Rc<Adapter>,
@@ -257,22 +330,26 @@ trait SensorPlugin {
     fn update(&mut self, storage: &mut PluginStorage) -> Result<(), Box<dyn Error>>;
 }
 
-/// I need:
-///   - A persistent storage of sensors (all metadata not expected to change during program lifetime)
-///     currently this means: key, adapter, and kind.
-///   - A temporary storage for the state of sensors
-///   - Access to those states by sensor key (e.g. for service & characteristics subcommands)
-///   - Mutable access to those states by sensor key (for updates)
-///   - Access to all sensors by plugin for plugin updates
+/// The central data holder of Groupie.
+/// Stores all sensor data as well as all [`SensorPlugin`] instances.
+/// 
+/// Groupie does not guarantee that only one instance of this struct will exist at a time
+/// and plugins should especially be able to handle being reinstantiated over the plugin lifetime.
 pub struct SensorStorage<'ctx> {
-    sensors: [(Box<dyn SensorPlugin + 'ctx>, PluginStorage); GroupiePluginType::COUNT],
+    /// An array of all plugins and their respective storages.
+    plugins: [(Box<dyn SensorPlugin + 'ctx>, PluginStorage); GroupiePluginType::COUNT],
+    /// A mapping of sensor keys to plugin types.
+    /// This is a cross-dependency with self.plugins as any key represented here must necessarily
+    /// appear in the PluginStorage of that plugin.
     sensor_to_plugin: HashMap<OwnedKey, GroupiePluginType>,
     has_discovered_sensors: bool,
 }
 impl<'ctx> SensorStorage<'ctx> {
+    /// Create a new storage with all active plugins initialised
+    /// and no stored data.
     pub fn new(context: &'ctx GlobalContext) -> Self {
         Self {
-            sensors: array::from_fn(|i| {
+            plugins: array::from_fn(|i| {
                 (
                     GroupiePluginType::VARIANTS[i].make_plugin(context),
                     PluginStorage::new(),
@@ -284,29 +361,35 @@ impl<'ctx> SensorStorage<'ctx> {
     }
 
     #[inline]
+    /// Get the [`PluginStorage`] for a particular sensor, if it exists within the database.
     fn get_storage_for<K: SensorKey>(&self, key: K) -> Option<&PluginStorage> {
         self.sensor_to_plugin
             .get(&key.get_sensor_key())
-            .map(|plugin| &self.sensors[*plugin as usize].1)
+            .map(|plugin| &self.plugins[*plugin as usize].1)
     }
 
+    /// Get the metadata of a sensor, if that sensor exists.
     pub fn get_sensor<K: SensorKey>(&self, key: K) -> Option<&Sensor> {
         let key = key.get_sensor_key();
         self.get_storage_for(key)
             .and_then(|storage| storage.get_sensor(key))
     }
 
+    /// Get the metadata and state data for a sensor combined.
+    /// 
+    /// Returns [`None`] if the sensor does not exist _or_ it does not have any data.
     pub fn get_sensor_data<T: SensorKey>(&self, key: T) -> Option<SensorData<'_>> {
         let key = key.get_sensor_key();
         self.get_storage_for(key)
             .and_then(|storage| storage.get(key))
     }
 
+    /// Repoll all sensors.
     pub fn update(&mut self) -> Result<(), ErrorGroup> {
         let mut errors = ErrorGroup::new();
         if !self.has_discovered_sensors {
             for (plugin_type, (plugin, plugin_storage)) in
-                GroupiePluginType::VARIANTS.iter().zip(&mut self.sensors)
+                GroupiePluginType::VARIANTS.iter().zip(&mut self.plugins)
             {
                 match plugin.discover_sensors(&mut |sensor| {
                     self.sensor_to_plugin
@@ -319,7 +402,7 @@ impl<'ctx> SensorStorage<'ctx> {
             }
             self.has_discovered_sensors = true;
         }
-        for (plugin, plugin_storage) in &mut self.sensors {
+        for (plugin, plugin_storage) in &mut self.plugins {
             if let Err(e) = plugin.update(plugin_storage) {
                 errors.push_box(e);
             }
@@ -331,7 +414,7 @@ impl<'ctx> SensorStorage<'ctx> {
     ///
     /// Note that if you only care about the updated sensors, [`Self::iter_data`] is preferrable.
     pub fn iter(&self) -> impl Iterator<Item = &(Sensor, Option<SensorState>)> {
-        self.sensors
+        self.plugins
             .iter()
             .map(|(_, storage)| storage)
             .flat_map(PluginStorage::iter)
@@ -350,6 +433,8 @@ impl<'ctx> SensorStorage<'ctx> {
 
 /// A wrapper struct that removes some of the functionalities of the internal HashMap.
 /// Used to ensure plugins cannot mess up the invariants required for internal datastructure integrity.
+/// 
+/// Most importantly, there is no simple `insert` function, due to the integrity constraint in [`SensorStorage`].
 pub struct PluginStorage {
     inner: HashMap<OwnedKey, (Sensor, Option<SensorState>)>,
 }
@@ -374,7 +459,6 @@ impl PluginStorage {
 
     // TODO: None represents two logical states here and in get_state.
     //  This is less than beautiful and may be better served by a ternary enum.
-
     /// Get the combined state of a sensor on this key, if it is available.
     /// If no sensor for the key exists, or it does not have a state, returns [`None`].
     /// Otherwise, returns a newly constructed [`SensorData`] instance (wrapped in [`Some`]).
@@ -387,7 +471,7 @@ impl PluginStorage {
     pub fn get_sensor<K: SensorKey>(&self, key: K) -> Option<&Sensor> {
         self.get_raw(&key).map(|(sensor, _)| sensor)
     }
-    pub fn put_sensor(&mut self, sensor: Sensor) -> Option<(Sensor, Option<SensorState>)> {
+    fn put_sensor(&mut self, sensor: Sensor) -> Option<(Sensor, Option<SensorState>)> {
         self.inner
             .insert(sensor.get_sensor_key().into(), (sensor, None))
     }
@@ -405,6 +489,9 @@ impl PluginStorage {
         Some(&mut self.get_raw_mut(&key)?.1)
     }
 
+    /// Inserts a [`SensorState`] for a sensor, referred to by `key`.
+    /// 
+    /// Returns an [`Err`] variant containing the passed state, if no sensor for that key exists.
     pub fn put_state<K: SensorKey>(
         &mut self,
         key: K,
@@ -416,6 +503,8 @@ impl PluginStorage {
         }
     }
 
+    /// Get references to both the sensor metadata as well as the sensor state,
+    /// allowing mutation on the state.
     pub fn get_both_mut<K: SensorKey>(
         &mut self,
         key: K,
@@ -432,7 +521,6 @@ impl Default for PluginStorage {
         Self::new()
     }
 }
-pub struct SensorIterator<'a, I: Iterator<Item = (&'a OwnedKey, SensorData<'a>)>>(I);
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq, strum::EnumCount, strum::VariantArray)]
 #[repr(u8)]
